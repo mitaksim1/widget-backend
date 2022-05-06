f2a95d089740471794fb665b2549b73f07037570
Miriam <miriamt.simonnet@gmail.com>
2022-05-06 17:01:52 +0200

    Fix bug : recéption image via Axios trop large

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,19 @@
 import express from 'express';
-import { routes } from './routes';
 import cors from 'cors';
+import { routes } from './routes';
 
 // Appel à Express
 const app = express();
 
 // Appel à cors
-app.use(cors());
+app.use(cors({
+    origin: 'http://localhost:3000'
+}));
 
 // Permet la récupération de la requête en format JSON
-app.use(express.json());
+// limit: '50mb' : nous permet d'envoyer une image dans le format base64 qui a une string trop large
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb' }));
 
 app.use(routes);
 
